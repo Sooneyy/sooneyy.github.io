f6fd5d48a3872f76dde5023cc5600fc983e6a979
Sooney <111024291+Sooneyy@users.noreply.github.com>
2024-01-28 23:44:33 +0100

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,51 @@
+window.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("year").textContent = new Date().getFullYear();
+
+    document.querySelectorAll(".info > .buttons > .button-bg > button").forEach(button => {
+        button.addEventListener("click", () => {
+            document.querySelector(".skills-items").innerHTML = "";
+
+            const type = button.dataset.skill;
+            const skills = data[type];
+
+            for(let item of skills){
+                const el = document.createElement("div");
+                const el2 = document.createElement("div");
+                const img = document.createElement("img");
+    
+                el.classList.add("skills-item");
+                document.querySelector(".skills-items").appendChild(el);
+    
+                el2.textContent = item.name;
+                el2.classList.add("tooltip");
+                el2.style.left = item.left + "px";
+                el2.setAttribute("aria-hidden", "false");
+                el.appendChild(el2);
+    
+                img.src = `assets/${item.name}.png`;
+                img.alt = item.name;
+                el.appendChild(img);
+
+                img.onmouseover = function(){
+                    el2.setAttribute("aria-hidden", "true");
+                }
+
+                img.onmouseleave = function(){
+                    el2.setAttribute("aria-hidden", "false");
+                }
+            }
+        })
+    })
+
+    document.querySelector("a[data-button='discord']").onclick = () => {
+        navigator.clipboard.writeText("sooney");
+        document.getElementById("copy").textContent = "Copied!";
+    }
+
+    document.querySelector("a[data-button='discord']").onmouseenter = () => {
+        document.getElementById("copy").setAttribute("aria-hidden", "true");
+        document.getElementById("copy").textContent = "Copy";
+    }
+
+    document.querySelector("a[data-button='discord']").onmouseleave = () => document.getElementById("copy").setAttribute("aria-hidden", "false");
+});
---
+++ js/data.js
@@ -0,0 +1,16 @@
+const data = {
+    "languages": [
+        {name: "HTML5", left: -7}, 
+        {name: "JavaScript", left: -19}, 
+        {name: "TypeScript", left: -19}, 
+        {name: "CSS3", left: -2}, 
+        {name: "PHP", left: 1}
+    ],
+    "technologies": [
+        {name: "NodeJS", left: -9}, 
+        {name: "VueJS", left: -4}, 
+        {name: "MySQL", left: -7}, 
+        {name: "React", left: -4}, 
+        {name: "WordPress", left: -20}
+    ]
+}
